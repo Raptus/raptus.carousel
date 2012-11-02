b333aee3f7cbb6cfb0bce519efc1fad943c29021
Jonas Wagner <jwagner@raptus.com>
2012-11-02 16:17:07 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b3'
 setup(name='raptus.carousel',
       version=version,
       description="Provides a jquery plugin to display a carousel",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.carousel',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.carousel',
+      url='https://github.com/Raptus/raptus.carousel',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus'],
